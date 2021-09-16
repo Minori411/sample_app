24ec0ae902ea46184282ccb785361d70df9bdac4
Minori411 <your>
2021-09-16 14:38:42 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get home" do
+    get root_path
     get static_pages_home_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
@@ -11,6 +12,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
+    get helf_path
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
@@ -18,9 +20,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
+    get about_path
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
 
+  end
+  test "should get contact" do
+    get contact_path
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
