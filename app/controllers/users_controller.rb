4e1137a31158d5fc58d6ce1b3cc6cd531eaaab07
Minori411 <your>
2021-09-20 13:20:04 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 include SessionsHelper
 before_action :logged_in_user, only: [:index, :edit, :update,:destroy]
 before_action :correct_user,   only: [:edit, :update]
-before_action :admin_user,     only: [:destroy]
+before_action :admin_user,     only: :destroy
 
 def index
-  @users = User.paginate(page: params[:page])
+  @users = User.paginate(page: params[:page])  
 end
   
 def show
