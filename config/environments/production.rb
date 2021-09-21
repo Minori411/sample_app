39ad72da00ed92e917f5b9957f7c64e16fb56c4a
Minori411 <your>
2021-09-21 16:35:27 +0900

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://www.minori411.com.herokuapp.com'
+  host = 'https://www.minori411.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
