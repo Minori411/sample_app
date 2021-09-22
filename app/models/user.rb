2e0e91ccc32de968fc1b6f66dd100f6734924c5a
Minori411 <your>
2021-09-22 12:18:08 +0900

    Add pg gem for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -11,37 +12,37 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 渡された文字列のハッシュ値を返す
-  def User.digest(string)
+def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-  end
+end
 
   # ランダムなトークンを返す
-  def User.new_token
+def User.new_token
     SecureRandom.urlsafe_base64
-  end
+end
 
   # 永続セッションのためにユーザーをデータベースに記憶する
-  def remember
+def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-  end
+end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(attribute, token)
+def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
-  end
+end
 
   # ユーザーのログイン情報を破棄する
-  def forget
+def forget
     update_attribute(:remember_digest, nil)
-  end
+end
 
  # アカウントを有効にする
- def activate
+def activate
   update_attribute(:activated,    true)
   update_attribute(:activated_at, Time.zone.now)
 end
@@ -64,20 +65,26 @@ def send_password_reset_email
 end
 
  # パスワード再設定の期限が切れている場合はtrueを返す
- def password_reset_expired?
+def password_reset_expired?
   reset_sent_at < 2.hours.ago
+end
+
+# 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+def feed
+    Micropost.where("user_id = ?", id)
 end
 
   private
 
    # メールアドレスをすべて小文字にする
-   def downcase_email
+def downcase_email
     self.email = email.downcase
-  end
+end
 
   # 有効化トークンとダイジェストを作成および代入する
-  def create_activation_digest
+def create_activation_digest
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
-  end
+end
 end
